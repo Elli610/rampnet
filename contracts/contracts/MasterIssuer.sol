1c82d35a6c4bd7b5fb1f7f47abfb817bd2d1e359
Elli <nathan@cypherlab.org>
2025-07-05 01:25:17 +0200

    test-flare contract

---
+++ contracts/contracts/MasterIssuer.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import {ContractRegistry} from "@flarenetwork/flare-periphery-contracts/coston2/ContractRegistry.sol";
+import {IWeb2Json} from "@flarenetwork/flare-periphery-contracts/coston2/IWeb2Json.sol";
+
+contract MasterIssuer {
+    uint256 public totalFacts;
+    
+    event LengthUpdated(uint256 indexed factId, uint256 length);
+
+    error InvalidProof();
+    
+    function isWeb2JsonProofValid(
+        IWeb2Json.Proof calldata _proof
+    ) private view returns (bool) {
+        return ContractRegistry.getFdcVerification().verifyJsonApi(_proof);
+    }
+    
+    function submitProof(IWeb2Json.Proof calldata proof) public {
+        require(isWeb2JsonProofValid(proof), InvalidProof());
+        
+        uint256 factLength = abi.decode(
+            proof.data.responseBody.abiEncodedData,
+            (uint256)
+        );
+        
+        totalFacts++;
+        
+        emit LengthUpdated(totalFacts, factLength);
+    }
+}
